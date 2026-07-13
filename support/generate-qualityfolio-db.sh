2529d4b11fa3b7be0d27a9bf6562174b25274206
Jerin George <116091529+jeringeor@users.noreply.github.com>
2026-07-13 18:54:55 +0530

    Refactor generate-qualityfolio-db.sh to remove exports
    Removed environment variable exports from the script.

--- support/generate-qualityfolio-db.sh
+++ support/generate-qualityfolio-db.sh
@@ -1,16 +1,5 @@
 cd support/assurance/qualityfolio
 
-cat > .envrc <<EOF
-export NOVU_API_KEY="$NOVU_API_KEY"
-export NOVU_API_URL="$NOVU_API_URL"
-export NOVU_WORKFLOW_ID="$NOVU_WORKFLOW_ID"
-export RECIPIENT_EMAIL="$RECIPIENT_EMAIL"
-export NOVU_EMAIL="$NOVU_EMAIL"
-export NOVU_PASSWORD="$NOVU_PASSWORD"
-EOF
-
-source .envrc
-
 spry rb run qualityfolio.md
 spry sp spc --fs dev-src.auto --destroy-first --conf sqlpage/sqlpage.json --md qualityfolio.md
 cat sqlpage/sqlpage.json
